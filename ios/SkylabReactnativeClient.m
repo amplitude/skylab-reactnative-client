2a42ed0de59f1eb6d8d9ff1f4f2bb7e20ffa9a43
haoliu <hao.liu@amplitude.com>
2021-03-31 16:38:10 -0700

    ios bridging naming fix

--- ios/SkylabReactnativeClient.m
+++ ios/SkylabReactnativeClient.m
@@ -1,6 +1,6 @@
 #import <React/RCTBridgeModule.h>
 
-@interface RCT_EXTERN_MODULE(AmplitudeReactNative, NSObject)
+@interface RCT_EXTERN_MODULE(SkylabReactnativeClient, NSObject)
 
 RCT_EXTERN_METHOD(initialize:(NSString *)apiKey config:(NSDictionary *)config resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 
