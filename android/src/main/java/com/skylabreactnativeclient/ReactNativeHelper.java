41166d40fdf322519ec2d0207cbbbe7d410fb46f
Curtis Liu <curtis@amplitude.com>
2021-04-01 11:01:00 -0700

    Add android native code

---
+++ android/src/main/java/com/skylabreactnativeclient/ReactNativeHelper.java
@@ -0,0 +1,138 @@
+package com.skylabreactnativeclient;
+
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableMapKeySetIterator;
+import com.facebook.react.bridge.ReadableType;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeArray;
+import com.facebook.react.bridge.WritableNativeMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+
+public class ReactNativeHelper {
+    /**
+     * This method will convert the input of type ReadableMap into the JSONObject.
+     */
+    private static final String UNSUPPORTED_TYPE = "Unsupported data type";
+
+    public static JSONObject convertMapToJson(ReadableMap value) throws JSONException {
+        JSONObject properties = new JSONObject();
+        ReadableMapKeySetIterator iterator = value.keySetIterator();
+        while (iterator.hasNextKey()) {
+            String key = iterator.nextKey();
+            ReadableType valueType = value.getType(key);
+            switch (valueType) {
+                case Null:
+                    properties.put(key, JSONObject.NULL);
+                    break;
+                case Boolean:
+                    properties.put(key, value.getBoolean(key));
+                    break;
+                case Number:
+                    properties.put(key, value.getDouble(key));
+                    break;
+                case String:
+                    properties.put(key, value.getString(key));
+                    break;
+                case Map:
+                    properties.put(key, convertMapToJson(value.getMap(key)));
+                    break;
+                case Array:
+                    properties.put(key, convertArrayToJson(value.getArray(key)));
+                    break;
+                default:
+                    throw new IllegalArgumentException(UNSUPPORTED_TYPE + valueType);
+            }
+        }
+        return properties;
+    }
+
+    /**
+     * This method will convert the input of type ReadableArray into the Json
+     * object.
+     */
+    public static JSONArray convertArrayToJson(ReadableArray value) throws JSONException {
+        JSONArray properties = new JSONArray();
+        for (int i = 0; i < value.size(); i++) {
+            ReadableType valueType = value.getType(i);
+            switch (valueType) {
+                case Null:
+                    properties.put(JSONObject.NULL);
+                    break;
+                case Boolean:
+                    properties.put(value.getBoolean(i));
+                    break;
+                case Number:
+                    properties.put(value.getDouble(i));
+                    break;
+                case String:
+                    properties.put(value.getString(i));
+                    break;
+                case Map:
+                    properties.put(convertMapToJson(value.getMap(i)));
+                    break;
+                case Array:
+                    properties.put(convertArrayToJson(value.getArray(i)));
+                    break;
+                default:
+                    throw new IllegalArgumentException(UNSUPPORTED_TYPE + valueType);
+            }
+        }
+        return properties;
+    }
+
+    public static WritableMap convertJsonToMap(JSONObject jsonObject) throws JSONException {
+        WritableMap map = new WritableNativeMap();
+        for (Iterator<String> it = jsonObject.keys(); it.hasNext(); ) {
+            String key = it.next();
+            Object value = jsonObject.get("it");
+            if (value == null) {
+                map.putNull(key);
+            } else if (value instanceof Boolean) {
+                map.putBoolean(key, (Boolean) value);
+            } else if (value instanceof Double) {
+                map.putDouble(key, (Double) value);
+            } else if (value instanceof Integer) {
+                map.putInt(key, (Integer) value);
+            } else if (value instanceof String) {
+                map.putString(key, (String) value);
+            } else if (value instanceof JSONObject) {
+                map.putMap(key, convertJsonToMap((JSONObject) value));
+            } else if (value instanceof JSONArray) {
+                map.putArray(key, convertJsonToMap((JSONArray) value));
+            }
+        }
+        return map;
+    }
+
+    public static WritableArray convertJsonToMap(JSONArray jsonArray) throws JSONException {
+        WritableArray array = new WritableNativeArray();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            Object value = jsonArray.get(i);
+            if (value == null) {
+                array.pushNull();
+            } else if (value instanceof Boolean) {
+                array.pushBoolean((Boolean) value);
+            } else if (value instanceof Double) {
+                array.pushDouble((Double) value);
+            } else if (value instanceof Integer) {
+                array.pushInt((Integer) value);
+            } else if (value instanceof String) {
+                array.pushString((String) value);
+            } else if (value instanceof JSONObject) {
+                array.pushMap(convertJsonToMap((JSONObject) value));
+            } else if (value instanceof JSONArray) {
+                array.pushArray(convertJsonToMap((JSONArray) value));
+            } else {
+                array.pushNull();
+            }
+        }
+        return array;
+    }
+}
---
+++ android/src/main/java/com/skylabreactnativeclient/SkylabReactNativeClientModule.java
@@ -0,0 +1,171 @@
+package com.skylabreactnativeclient;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+
+import com.amplitude.api.Amplitude;
+import com.amplitude.api.AmplitudeContextProvider;
+import com.amplitude.skylab.Skylab;
+import com.amplitude.skylab.SkylabClient;
+import com.amplitude.skylab.SkylabConfig;
+import com.amplitude.skylab.SkylabUser;
+import com.amplitude.skylab.Variant;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableType;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeMap;
+import com.facebook.react.module.annotations.ReactModule;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Map;
+import java.util.concurrent.Future;
+
+@ReactModule(name = SkylabReactNativeClientModule.NAME)
+public class SkylabReactNativeClientModule extends ReactContextBaseJavaModule {
+    public static final String NAME = "SkylabReactNativeClient";
+    private ReactApplicationContext reactContext;
+
+    public SkylabReactNativeClientModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    @Override
+    @NonNull
+    public String getName() {
+        return NAME;
+    }
+
+
+    // Example method
+    // See https://reactnative.dev/docs/native-modules-android
+    @ReactMethod
+    public void init(String apiKey, ReadableMap config, Promise promise) throws JSONException {
+        Variant fallbackVariant = null;
+        if (config.getType("fallbackVariant") == ReadableType.String) {
+            fallbackVariant = new Variant(config.getString("fallbackVariant"));
+        } else if (config.getType("fallbackVariant") == ReadableType.Map) {
+            fallbackVariant =
+                    Variant.fromJsonObject(ReactNativeHelper.convertMapToJson(config.getMap(
+                    "fallbackVariant")));
+        }
+        SkylabConfig convertedConfig = SkylabConfig.builder()
+                .setFallbackVariant(fallbackVariant)
+                .setServerUrl(config.getString("serverUrl")).build();
+        Skylab.init((Application) this.reactContext.getApplicationContext(), apiKey,
+                convertedConfig);
+    }
+
+    @ReactMethod
+    public void start(ReadableMap user, Promise promise) throws JSONException {
+        Future<SkylabClient> future = Skylab.getInstance().start(convertSkylabUser(user));
+        promise.resolve(true);
+    }
+
+    @ReactMethod
+    public void setUser(ReadableMap user, Promise promise) throws JSONException {
+        Future<SkylabClient> future = Skylab.getInstance().setUser(convertSkylabUser(user));
+        promise.resolve(true);
+    }
+
+    @ReactMethod
+    public void getVariant(String flagKey, Promise promise) throws JSONException {
+        Variant variant = Skylab.getInstance().getVariant(flagKey);
+        promise.resolve(variantToMap(variant));
+    }
+
+    @ReactMethod
+    public void getVariant(String flagKey, ReadableMap fallback, Promise promise) throws JSONException {
+        Variant fallbackVariant =
+                Variant.fromJsonObject(ReactNativeHelper.convertMapToJson(fallback));
+        Variant variant = Skylab.getInstance().getVariant(flagKey, fallbackVariant);
+        promise.resolve(variantToMap(variant));
+    }
+
+    @ReactMethod
+    public void getVariant(String flagKey, String fallback, Promise promise) throws JSONException {
+        Variant fallbackVariant = new Variant(fallback);
+        Variant variant = Skylab.getInstance().getVariant(flagKey, fallbackVariant);
+        promise.resolve(variantToMap(variant));
+    }
+
+    @ReactMethod
+    public void getVariants(Promise promise) throws JSONException {
+        WritableMap map = new WritableNativeMap();
+        Map<String, Variant> variants = Skylab.getInstance().getVariants();
+        for (Map.Entry<String, Variant> entry : variants.entrySet()) {
+            map.putMap(entry.getKey(), variantToMap(entry.getValue()));
+        }
+        promise.resolve(map);
+    }
+
+    @ReactMethod
+    public void refetchAll(Promise promise) {
+        Future<SkylabClient> future = Skylab.getInstance().refetchAll();
+        promise.resolve(true);
+    }
+
+    @ReactMethod
+    public void setContextProvider(String amplitudeInstanceName, Promise promise) {
+        Skylab.getInstance().setContextProvider(new AmplitudeContextProvider(Amplitude.getInstance(amplitudeInstanceName)));
+        promise.resolve(true);
+    }
+
+    @ReactMethod
+    public void setListener(Callback listener) {
+        Skylab.getInstance().setListener((skylabUser, variants) -> {
+            try {
+                WritableMap map = new WritableNativeMap();
+                for (Map.Entry<String, Variant> entry : variants.entrySet()) {
+                    map.putMap(entry.getKey(), variantToMap(entry.getValue()));
+                }
+                listener.invoke(map);
+            } catch (JSONException e) {
+
+            }
+        });
+    }
+
+    // Conversion methods
+    private SkylabUser convertSkylabUser(ReadableMap user) throws JSONException {
+        SkylabUser convertedUser = SkylabUser.builder()
+                .setDeviceId(user.getString(SkylabUser.DEVICE_ID))
+                .setUserId(user.getString(SkylabUser.USER_ID))
+                .setVersion(user.getString(SkylabUser.VERSION))
+                .setCountry(user.getString(SkylabUser.COUNTRY))
+                .setRegion(user.getString(SkylabUser.REGION))
+                .setDma(user.getString(SkylabUser.DMA))
+                .setCity(user.getString(SkylabUser.CITY))
+                .setLanguage(user.getString(SkylabUser.LANGUAGE))
+                .setPlatform(user.getString(SkylabUser.PLATFORM))
+                .setOs(user.getString(SkylabUser.OS))
+                .setDeviceFamily(user.getString(SkylabUser.DEVICE_FAMILY))
+                .setDeviceType(user.getString(SkylabUser.DEVICE_TYPE))
+                .setDeviceBrand(user.getString(SkylabUser.DEVICE_BRAND))
+                .setDeviceManufacturer(user.getString(SkylabUser.DEVICE_MANUFACTURER))
+                .setDeviceModel(user.getString(SkylabUser.DEVICE_MODEL))
+                .setCarrier(user.getString(SkylabUser.CARRIER))
+                .setLibrary(user.getString(SkylabUser.LIBRARY))
+                .setUserProperties(ReactNativeHelper.convertMapToJson(user.getMap(SkylabUser.USER_PROPERTIES))).build();
+        return convertedUser;
+    }
+
+    private WritableMap variantToMap(Variant variant) throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        if (variant.value != null) {
+            jsonObject.put("value", variant.value);
+        }
+        if (variant.payload != null) {
+            jsonObject.put("payload", variant.payload);
+        }
+        return ReactNativeHelper.convertJsonToMap(jsonObject);
+    }
+}
