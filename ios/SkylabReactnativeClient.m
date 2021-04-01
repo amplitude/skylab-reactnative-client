56fb2a4681fd861189dd5fad94a1dfddf32258df
haoliu <hao.liu@amplitude.com>
2021-03-31 22:45:35 -0700

    Add 1st batch ios apis to enable tests

--- ios/SkylabReactnativeClient.m
+++ ios/SkylabReactnativeClient.m
@@ -4,4 +4,12 @@
 
 RCT_EXTERN_METHOD(initialize:(NSString *)apiKey resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 
+RCT_EXTERN_METHOD(start:(RCTResponseSenderBlock)callback resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(setUser:(NSDictionary *)user callback:(RCTResponseSenderBlock)callback resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(getVariant:(NSString *)flagKey callback:(RCTResponseSenderBlock)callback resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(getVariants: (RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+
 @end
--- ios/SkylabReactnativeClient.swift
+++ ios/SkylabReactnativeClient.swift
@@ -14,4 +14,78 @@ class SkylabReactNativeClient: NSObject {
         let config = SkylabConfig()
         let _ = Skylab.initialize(apiKey: apiKey, config: config)
     }
+
+    // REMAINING ONES
+//    public void getVariant(String flagKey, ReadableMap fallback, Promise promise)
+//    public void getVariant(String flagKey, String fallback, Promise promise)
+//    public void refetchAll(Promise promise)
+    
+    @objc
+    func start(_ callback: RCTResponseSenderBlock?,
+               resolver resolve: RCTPromiseResolveBlock,
+               rejecter reject: RCTPromiseRejectBlock) -> Void {
+        let user = SkylabUser()
+        Skylab.getInstance()?.start(user: user, completion: {
+            callback?(["success"])
+        })
+    }
+    
+    @objc
+    func setUser(_ user: [String: Any],
+                 callback: RCTResponseSenderBlock?,
+                 resolver resolve: RCTPromiseResolveBlock,
+                 rejecter reject: RCTPromiseRejectBlock) -> Void {
+        let u = SkylabUser(deviceId: user["device_id"] as! String?,
+                           userId: user["user_id"] as! String?,
+                           version: user["version"] as! String?,
+                           country: user["country"] as! String?,
+                           region: user["region"] as! String?,
+                           dma: user["dma"] as! String?,
+                           city: user["city"] as! String?,
+                           language: user["language"] as! String?,
+                           platform: user["platform"] as! String?,
+                           os: user["os"] as! String?,
+                           deviceFamily: user["device_family"] as! String?,
+                           deviceType: user["device_type"] as! String?,
+                           deviceManufacturer: user["device_manufacturer"] as! String?,
+                           deviceModel: user["device_model"] as! String?,
+                           carrier: user["carrier"] as! String?,
+                           userProperties: user["user_properties"] as! [String: String]?)
+
+        Skylab.getInstance()?.setUser(user: u, completion: {
+            callback?(["success"])
+        })
+    }
+    
+    @objc
+    func getVariant(_ flagKey: String,
+                    callback: RCTResponseSenderBlock?,
+                    resolver resolve: RCTPromiseResolveBlock,
+                    rejecter reject: RCTPromiseRejectBlock) -> Void {
+        let variant = Skylab.getInstance()?.getVariant(flagKey, fallback: nil)
+        
+        var variantMap = [String: Any]()
+        variantMap["value"] = variant?.value
+        variantMap["payload"] = variant?.payload
+        resolve(variantMap)
+    }
+    
+    @objc
+    func getVariants(resolver resolve: RCTPromiseResolveBlock,
+                     rejecter reject: RCTPromiseRejectBlock) -> Void {
+        let variants = Skylab.getInstance()?.getVariants()
+        
+        if let variantsList = variants {
+            var map = [String: Any]()
+            for (key,value) in variantsList {
+                var variantMap = [String: Any]()
+                variantMap["value"] = value.value
+                variantMap["payload"] = value.payload
+                map[key] = variantMap
+            }
+            resolve(map)
+        } else {
+            resolve(nil)
+        }
+    }
 }
